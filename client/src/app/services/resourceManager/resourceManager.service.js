d02e7e1da8f13f1f256e290ffd1cb41bf2781198
saskodh <sasko_dh@hotmail.com>
2014-11-17 00:09:10 +0100

    resourceManager service implemented   - resourceFactory   - resourceParser

--- client/src/app/services/resourceManager/resourceManager.service.js
+++ client/src/app/services/resourceManager/resourceManager.service.js
@@ -1,212 +1,58 @@
 'use strict';
 
 angular.module('sbApp')
-  .factory('resourceManager', function(navigator, _) {
-    //NOTE: we can use this for caching also
-    var _rawResource = null;
-    var _parsed = false;
-    var _graph = null;
-    var _literals = [];
-    var _resourceShortInfo = null;
+  .factory('resourceManager', function(sbEndpoint, resourceParser, SB_APP_EVENTS, sbEventBus, _) {
 
-    var setRawResource = function(rawResource) {
-
-      _rawResource = rawResource;
-      _parsed = false;
+    var resource = {
+      uri: '',
+      description: {},
+      literals: [],
+      graphData: {}
     };
 
-    var extractNameFromUri = function (uri) {
-      return uri.substring(uri.lastIndexOf('/')+1, uri.length);
+    var resourceObservers = [];
+    var registerResourceObserver = function (listener) {
+      resourceObservers.push(listener);
     };
-
-    var extractFirstSentence = function (text) {
-      return text.substring(0, text.indexOf('.'));
-    };
-
-    var Node = function (uri, isMainNode, isPredicateNode) {
-      this.uri = uri;
-      this.isMainNode = isMainNode;
-      this.isPredicateNode = isPredicateNode;
-      this.name = extractNameFromUri(uri);
-      this.childs = [];
-    };
-    var Link = function (source, target, isSourcePredicate) {
-      this.source = source;
-      this.target = target;
-      this.isSourcePredicate = isSourcePredicate;
-    };
-    var LiteralNode = function (uriPredicate) {
-      this.uriPredicate = uriPredicate;
-      this.name = extractNameFromUri(uriPredicate);
-      this.literals = [];
-    };
-    var Literal = function (language, value) {
-      this.language = language;
-      this.value = value;
-    };
-
-    var getRawResource = function() {
-      return _rawResource;
-    };
-
-    var parseAndExtractData = function() {
-      //TODO: try to parse the data
-      _literals = [];
-      _graph = {
-        nodes: [],
-        links: [],
-        mainNode: null
-      };
-
-      var resourceHashMap = {};
-      var literalHashMap = {};
-
-      _.forOwn(_rawResource, function (i, uriResource) {
-        var object = _rawResource[uriResource];
-        var node, link;
-        //create node for each resource
-        //add it to the hash if does not exists
-        if(!_.has(resourceHashMap, uriResource)) {
-          node = new Node(uriResource, uriResource === navigator.current(), false);
-          resourceHashMap[uriResource] = node;
-          _graph.nodes.push(node);
-        }
-
-        // for all his predicates
-        _.forOwn(object, function (j, uriPredicate) {
-
-          var hasResourceFlag = false;
-          // for each subject
-          _.forEach(object[uriPredicate], function (subject) {
-            if (subject.type === 'uri') {
-              hasResourceFlag = true;
-              // if predicate doesnt exists (on the first uri subject)
-              if(!_.has(resourceHashMap, uriPredicate)) {
-                var pred = new Node(uriPredicate, false, true);
-                resourceHashMap[uriPredicate] = pred;
-                _graph.nodes.push(pred);
-              }
-
-              // if subject doesn't exists
-              if(!_.has(resourceHashMap, subject.value)) {
-                var sub = new Node(subject.value, subject.value === navigator.current(), false);
-                resourceHashMap[sub.uri] = sub;
-                _graph.nodes.push(sub);
-              }
-
-              // add link
-              link = new Link(resourceHashMap[subject.value], resourceHashMap[uriPredicate], false);
-              // create tree structure
-              if (link.source.isMainNode) {
-                link.source.childs.push(link.target);
-              } else {
-                link.target.childs.push(link.source);
-              }
-              _graph.links.push(link);
-            }
-
-            if (subject.type === 'literal') {
-              // if predicate doesnt exists (on the first literal subject)
-              if(!_.has(literalHashMap, uriPredicate)) {
-                var literalNode = new LiteralNode(uriPredicate);
-                literalHashMap[uriPredicate] = literalNode;
-                _literals.push(literalNode);
-              }
-
-              literalHashMap[uriPredicate].literals.push(new Literal(subject.lang, subject.value));
-            }
-          });
-          //create node for each predicate that has at least one resource
-          if (hasResourceFlag) {
-            //add it to the hash if does not exists
-            if(!_.has(resourceHashMap, uriPredicate)) {
-              node = new Node(uriPredicate, false, true);
-              resourceHashMap[uriPredicate] = node;
-              _graph.nodes.push(node);
-            }
-
-            //create link
-            link = new Link(resourceHashMap[uriPredicate], resourceHashMap[uriResource], false);
-            // create tree structure
-            if (link.target.isMainNode) {
-              link.target.childs.push(link.source);
-            } else {
-              link.source.childs.push(link.target);
-            }
-            _graph.links.push(link);
-          }
-        });
-
+    var notifyResourceObservers = function () {
+      _.forEach(resourceObservers, function (observerCallback) {
+        observerCallback();
       });
-
-      // create resource short info
-      createResourceShortInfo(resourceHashMap);
-
-      // set main node
-      _graph.mainNode = resourceHashMap[navigator.current()];
-      _graph.mainNode.image = _resourceShortInfo.image;
-
-      //after parsing
-      _parsed = true;
     };
 
-    var createResourceShortInfo = function (resourceHash) {
-      _resourceShortInfo = {};
+    var getResourceUri = function () {
+      return resource.uri;
+    };
+    var getResourceDescription = function () {
+      return resource.description;
+    };
+    var getResourceLiterals = function () {
+      return resource.literals;
+    };
+    var getResourceGraphData = function () {
+      return resource.graphData;
+    };
 
-      _literals.forEach(function (literal) {
-        if (literal.name === 'label') {
-          literal.literals.forEach(function (literalValue) {
-            if (literalValue.language === 'en') {
-              _resourceShortInfo.name = literalValue.value;
-            }
-          });
-        }
+    var loadResource = function (resourceUri) {
+      sbEventBus.triggerEvent(SB_APP_EVENTS.RESOURCE_LOAD_START, resourceUri);
+      resource.uri = resourceUri;
 
-        if (literal.name === 'abstract') {
-          literal.literals.forEach(function (literalValue) {
-            if (literalValue.language === 'en') {
-              _resourceShortInfo.shortDescription = extractFirstSentence(literalValue.value);
-            }
-          });
-        }
+      sbEndpoint.getResource(resourceUri).then(function (rawResource) {
+        resource = resourceParser.parseResource(rawResource, resource.uri);
+        notifyResourceObservers();
+      }, function (error) {
+
+      }).finally(function () {
+        sbEventBus.triggerEvent(SB_APP_EVENTS.RESOURCE_LOAD_END);
       });
-
-      // TODO: make this generic
-      if (resourceHash['http://dbpedia.org/ontology/thumbnail']) {
-        _resourceShortInfo.image = resourceHash['http://dbpedia.org/ontology/thumbnail'].childs[0].uri;
-      }
-    };
-
-    var getLiterals = function() {
-      if(_parsed) {
-        return _literals;
-      } else {
-        throw 'ResourceNotParsedException';
-      }
-    };
-
-    var getGraph = function() {
-      if(_parsed) {
-        return _graph;
-      } else {
-        throw 'ResourceNotParsedException';
-      }
-    };
-
-    var getResourceShortInfo = function () {
-      if(_parsed) {
-        return _resourceShortInfo;
-      } else {
-        throw 'ResourceNotParsedException';
-      }
     };
 
     return {
-      setRawResource: setRawResource,
-      getRawResource: getRawResource,
-      parseAndExtractData: parseAndExtractData,
-      getGraph: getGraph,
-      getLiterals: getLiterals,
-      getResourceShortInfo: getResourceShortInfo
+      loadResource: loadResource,
+      registerResourceObserver: registerResourceObserver,
+      getResourceUri: getResourceUri,
+      getResourceDescription: getResourceDescription,
+      getResourceLiterals: getResourceLiterals,
+      getResourceGraphData: getResourceGraphData
     };
   });
---
+++ client/src/app/services/resourceManager/utils/resourceFactory/resourceFactory.service.js
@@ -0,0 +1,66 @@
+'use strict';
+
+angular.module('sbApp')
+
+  .constant('RESOURCE_TYPE', {
+    'RESOURCE': 'resource',
+    'MAIN_RESOURCE': 'main_resource',
+    'PREDICATE': 'predicate',
+    'LITERAL_NODE': 'literal_node',
+    'LITERAL': 'literal',
+    'LINK': 'link'
+  })
+
+  .factory('resourceFactory', function (RESOURCE_TYPE) {
+
+    var extractResourceName = function (resourceUri) {
+      return resourceUri.substring(resourceUri.lastIndexOf('/')+1, resourceUri.length);
+    };
+
+    var createResource = function (resourceUri) {
+      return {
+        uri: resourceUri,
+        name: extractResourceName(resourceUri),
+        type: RESOURCE_TYPE.RESOURCE
+      }
+    };
+    var createMainResource = function (resourceUri) {
+      var mainResource = createResource(resourceUri);
+      mainResource.type = RESOURCE_TYPE.MAIN_RESOURCE;
+      return mainResource;
+    };
+    var createPredicate = function (predicateUri) {
+      var predicate = createResource(predicateUri);
+      predicate.type = RESOURCE_TYPE.PREDICATE;
+      return predicate;
+    };
+    var createLiteralNode = function (predicateUri) {
+      var literalNode = createResource(predicateUri);
+      literalNode.type = RESOURCE_TYPE.LITERAL_NODE;
+      literalNode.literals = [];
+      return literalNode;
+    };
+    var createLiteral = function (text, language) {
+      return {
+        type: RESOURCE_TYPE.LITERAL,
+        text: text,
+        language: language
+      }
+    };
+    var createLink = function (source, target) {
+      return {
+        type: RESOURCE_TYPE.LINK,
+        source: source,
+        target: target
+      }
+    };
+
+    return {
+      createResource: createResource,
+      createMainResource: createMainResource,
+      createPredicate: createPredicate,
+      createLiteralNode: createLiteralNode,
+      createLiteral: createLiteral,
+      createLink: createLink
+    };
+  });
