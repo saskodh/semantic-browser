11ad6a7216ea2b2404a0159ffcaeba81f1286b69
saskodh <sasko_dh@hotmail.com>
2014-11-21 19:04:37 +0100

    sbAboutModal dialog added without implementation

---
+++ client/src/app/partials/sbAboutModal/sbAboutModal.directive.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('sbApp')
+  .directive('sbAboutModal', function () {
+    return {
+      templateUrl: 'src/app/partials/sbAboutModal/sbAboutModal.directive.tpl.html',
+      scope: {},
+      controller: 'SbAboutModalController',
+      link: function (scope, element) {
+        // NOTE: in case we want to style the container element
+        element.addClass('sb-about-modal');
+      }
+    }
+  });
